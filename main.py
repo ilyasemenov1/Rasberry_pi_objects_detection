2798f4bf9b55bab4322467f5f83724e0eae74336
ilyasemenov1 <sialogacc@gmail.com>
2022-07-27 03:16:48 -0900

    Init commit

---
+++ main.py
@@ -0,0 +1,68 @@
+from picamera.array import PiRGBArray
+from picamera import PiCamera 
+import time
+import cv2
+import json
+import string_int_label_map_pb2
+import label_map_util
+import tf_labels
+import project_metods
+
+class Camera():
+    
+    def __init__(self):
+        self.cam = PiCamera()
+        
+        #with open("camera_settings.json", "r") as read_file:
+        #    data = json.load(read_file)
+        #    print(data)
+        
+        self.cam.resolution = (304, 304)
+        self.cam.framerate = 32
+        self.cam.sharpness = 0
+        self.cam.contrast = 0
+        self.cam.brightness = 50
+        self.cam.saturation = 0
+        self.cam.ISO = 0
+        self.cam.video_stabilization = True
+        self.cam.exposure_compensation = 0
+        self.cam.exposure_mode = 'auto'
+        self.cam.meter_mode = 'average'
+        self.cam.awb_mode = 'auto'
+        self.cam.image_effect = 'none'
+        self.cam.color_effects = None
+        self.cam.rotation = 0
+        self.cam.hflip = False
+        self.cam.vflip = False
+        self.cam.crop = (0.0, 0.0, 1.0, 1.0)
+        
+        self.raw_capture = PiRGBArray(self.cam, size=(304, 304))
+        
+        self.key = None
+        self.img = None
+        
+        time.sleep(0.1)
+        
+    def video(self):
+        
+        for frame in self.cam.capture_continuous(self.raw_capture, format="bgr", use_video_port=True):
+            self.capture(frame)
+            
+            if self.key == ord("q"):
+                break
+        
+    def capture(self, cap):
+        self.img = cap.array
+    
+        self.img = project_metods.Main(self.img).object_recognition()
+    
+        cv2.imshow("Frame", self.img)
+    
+        self.key = cv2.waitKey(1) & 0xFF
+        self.raw_capture.truncate(0)
+    
+        
+if __name__ == "__main__":
+
+    camera = Camera()
+    camera.video()
---
+++ project_metods.py
@@ -0,0 +1,35 @@
+import sys
+import time
+import cv2
+import tf_labels
+
+tf_labels.initLabels("opencv-extra/mscoco_label_map.pbtxt")
+cvNet = cv2.dnn.readNetFromTensorflow("resourses/frozen_inference_graph.pb", "opencv-extra/ssd_mobilenet_v1_coco.pbtxt")
+
+
+class Main:
+    
+    def __init__(self, img):
+        self.img = img
+
+        
+    def object_recognition(self):
+        rows = self.img.shape[0]
+        cols = self.img.shape[1]
+        cvNet.setInput(cv2.dnn.blobFromImage(self.img, 1.0/127.5, (300, 300), (127.5, 127.5, 127.5), swapRB=True, crop=False))
+        cvOut = cvNet.forward()
+
+        for detection in cvOut[0,0,:,:]:
+            score = float(detection[2])
+            if score > 0.25:
+                left = int(detection[3] * cols)
+                top = int(detection[4] * rows)
+                right = int(detection[5] * cols)
+                bottom = int(detection[6] * rows)
+                label = tf_labels.getLabel(int(detection[1]))
+                print(label, score, left, top, right, bottom)
+                text_color = (23, 230, 210)
+                cv2.rectangle(self.img, (left, top), (right, bottom), text_color, thickness=2)
+                cv2.putText(self.img, label, (left, top), cv2.FONT_HERSHEY_SIMPLEX, 1, text_color, 2)
+                
+        return self.img
